c46dab689493b31c4d4908e05f19c7d5cbbc182f
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-07-18 12:49:51 -0700

    Defined api, pricing, sign-in/sign-up routes

---
+++ nextjs-app/src/app/api/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+// export async function GET(request: Request) {
+
+// }
+
+export async function GET() {
+  return NextResponse.json({
+    message: "API working!",
+  });
+}
---
+++ nextjs-app/src/app/pricing/page.tsx
@@ -0,0 +1,152 @@
+export default function PricingPage() {
+  return (
+    <div className="p-12 lg:px-64">
+      <div className="flex justify-center flex-col items-center">
+        <h1 className="text-indigo-500 font-semibold">Pricing</h1>
+        <p className="mt-1 font-bold text-4xl">
+          Pricing plans for every budget.
+        </p>
+        <p className="mt-2 text-gray-600">This is the pricing page.</p>
+      </div>
+      <ul className="flex flex-row justify-center mt-20">
+        <li className="border border-l-gray-300 border-y-gray-300 rounded-3xl p-8 flex justify-center flex-col mr-10">
+          <div>
+            <h1 className="font-semibold text-xl">Free</h1>
+            <p className="mt-4">Essentials to get started.</p>
+            <h2 className="font-bold text-3xl mt-4">
+              $0
+              <span className="font-medium text-sm text-gray-600">/month</span>
+            </h2>
+            <ul className="mt-6">
+              <li className="flex flex-row items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-5 mr-2 text-indigo-500"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+                <p className="text-sm">Feature 1</p>
+              </li>
+              <li className="flex flex-row items-center mt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-5 mr-2 text-indigo-500"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+                <p className="text-sm">Feature 2</p>
+              </li>
+              <li className="flex flex-row items-center mt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-5 mr-2 text-indigo-500"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+                <p className="text-sm">Feature 3</p>
+              </li>
+            </ul>
+          </div>
+          <button className="mt-20 border hover:border-blue-500 transition border-gray-300 text-blue-600 text-sm font-semibold py-2 px-4 rounded-lg">
+            Get Started
+          </button>
+        </li>
+        <li className="border-2 border-blue-500 rounded-3xl p-8 flex justify-center flex-col">
+          <div>
+            <div className="flex flex-row justify-between items-center">
+              <h1 className="font-semibold text-xl text-blue-500">Free</h1>
+              <div className="font-semibold text-xs text-blue-500 bg-blue-200 px-2 py-1 rounded-xl">
+                most popular
+              </div>
+            </div>
+            <p className="mt-4">Essentials to get started.</p>
+            <h2 className="font-bold text-3xl mt-4">
+              $0
+              <span className="font-medium text-sm text-gray-600">/month</span>
+            </h2>
+            <ul className="mt-6">
+              <li className="flex flex-row items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-5 mr-2 text-blue-500"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+                <p className="text-sm">Feature 1</p>
+              </li>
+              <li className="flex flex-row items-center mt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-5 mr-2 text-blue-500"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+                <p className="text-sm">Feature 2</p>
+              </li>
+              <li className="flex flex-row items-center mt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-5 mr-2 text-blue-500"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.5 12.75l6 6 9-13.5"
+                  />
+                </svg>
+                <p className="text-sm">Feature 3</p>
+              </li>
+            </ul>
+          </div>
+          <button className="mt-20 border hover:border-blue-500 transition border-gray-300 text-blue-600 text-sm font-semibold py-2 px-4 rounded-lg">
+            Get Started
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
