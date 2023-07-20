fca55e448a3866423de3538bef71178c967bf702
Adrian <adrianlam1511@gmail.com>
2023-07-20 15:36:36 -0700

    Prettier format and tool dropdown

--- nextjs-app/src/components/navbar.tsx
+++ nextjs-app/src/components/navbar.tsx
@@ -1,9 +1,26 @@
 "use client";
 
-import { Navbar, Text, Button, Link } from "@nextui-org/react";
+import { Navbar, Text, Button, Link, Dropdown } from "@nextui-org/react";
 import AcademicCapIcon from "@heroicons/react/24/outline/AcademicCapIcon";
 
-const collapseItems = ["Home", "Features", "Pricing", "Help and Feedback"];
+const collapseItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Tools",
+    href: "/tools",
+  },
+  {
+    title: "Pricing",
+    href: "/pricing",
+  },
+  {
+    title: "Help and Feedback",
+    href: "/help-and-feedback",
+  },
+];
 
 export default function NavbarComp() {
   return (
@@ -23,8 +40,75 @@ export default function NavbarComp() {
           <Navbar.Link isActive href="/">
             Home
           </Navbar.Link>
-          <Navbar.Link href="/">Features</Navbar.Link>
-
+          <Dropdown isBordered>
+            <Navbar.Item>
+              <Dropdown.Button auto light ripple={false}>
+                Tools
+              </Dropdown.Button>
+            </Navbar.Item>
+            <Dropdown.Menu
+              aria-label="ACME features"
+              css={{
+                $$dropdownMenuWidth: "340px",
+                $$dropdownItemHeight: "70px",
+                "& .nextui-dropdown-item": {
+                  py: "$4",
+                  // dropdown item left icon
+                  svg: {
+                    color: "$secondary",
+                    mr: "$4",
+                  },
+                  // dropdown item title
+                  "& .nextui-dropdown-item-content": {
+                    w: "100%",
+                    fontWeight: "$semibold",
+                  },
+                },
+              }}
+            >
+              <Dropdown.Item
+                key="autoscaling"
+                showFullDescription
+                description="ACME scales apps to meet user demand, automagically, based on load."
+                // icon={icons.scale}
+              >
+                Autoscaling
+              </Dropdown.Item>
+              <Dropdown.Item
+                key="usage_metrics"
+                showFullDescription
+                description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
+                // icon={icons.activity}
+              >
+                Usage Metrics
+              </Dropdown.Item>
+              <Dropdown.Item
+                key="production_ready"
+                showFullDescription
+                description="ACME runs on ACME, join us and others serving requests at web scale."
+                // icon={icons.flash}
+              >
+                Production Ready
+              </Dropdown.Item>
+              <Dropdown.Item
+                key="99_uptime"
+                showFullDescription
+                description="Applications stay on the grid with high availability and high uptime guarantees."
+                // icon={icons.server}
+              >
+                +99% Uptime
+              </Dropdown.Item>
+              <Dropdown.Item
+                key="supreme_support"
+                showFullDescription
+                description="Overcome any challenge with a supporting team ready to respond."
+                // icon={icons.user}
+              >
+                +Supreme Support
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          {/* <Navbar.Link href="/">Tools</Navbar.Link> */}
           <Navbar.Link href="/pricing">Pricing</Navbar.Link>
           <Navbar.Link href="/help-and-feedback">FAQ</Navbar.Link>
         </Navbar.Content>
@@ -40,15 +124,15 @@ export default function NavbarComp() {
         </Navbar.Content>
         <Navbar.Collapse>
           {collapseItems.map((item, index) => (
-            <Navbar.CollapseItem key={item}>
+            <Navbar.CollapseItem key={item.title}>
               <Link
                 color="inherit"
                 css={{
                   minWidth: "100%",
                 }}
-                href="#"
+                href={item.href}
               >
-                {item}
+                {item.title}
               </Link>
             </Navbar.CollapseItem>
           ))}
