e6e6eb41e96f2c98a7c9a0fffde94efe78c79ee4
Adrian <adrianlam1511@gmail.com>
2023-07-24 00:08:21 -0700

    Added vercel analytics

--- nextjs-app/src/pages/_app.tsx
+++ nextjs-app/src/pages/_app.tsx
@@ -1,12 +1,14 @@
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // 2. Use at the root of your app
     <NextUIProvider>
       <Component {...pageProps} />
+      <Analytics />
     </NextUIProvider>
   );
 }
